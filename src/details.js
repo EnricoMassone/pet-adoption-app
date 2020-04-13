d7724b05a2151090f796b0a99a0186f229830a3a
Enrico Massone <enrico.massone@deltatre.it>
2020-04-13 16:54:12 +0200

    added the route for pet details page

---
+++ src/details.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Details extends React.Component {
+  render() {
+    return <h2>This is the details page for id {this.props.id}</h2>;
+  }
+}
+
+export default Details;

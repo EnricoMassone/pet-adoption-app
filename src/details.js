b64b40f8f1f8e328afe2d2c84ef80f30fa2f5065
Enrico Massone <enrico.massone@deltatre.it>
2020-04-13 19:43:21 +0200

    builded up the detail page

--- src/details.js
+++ src/details.js
@@ -1,8 +1,68 @@
 import React from "react";
+import petfinder from "petfinder-client";
+
+const api = petfinder({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET,
+});
 
 class Details extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    const promise = api.pet.get({
+      output: "full",
+      id: this.props.id,
+    });
+
+    promise.then((data) => {
+      const pet = data.petfinder.pet;
+
+      let breed;
+
+      if (Array.isArray(pet.breeds.breed)) {
+        breed = pet.breeds.breed.join(", ");
+      } else {
+        breed = pet.breeds.breed;
+      }
+
+      const { name, animal, description, media, contact } = pet;
+      const location = `${contact.city}, ${contact.state}`;
+
+      this.setState({
+        loading: false,
+        name,
+        animal,
+        description,
+        media,
+        location,
+        breed,
+      });
+    });
+  }
+
   render() {
-    return <h2>This is the details page for id {this.props.id}</h2>;
+    if (this.state.loading) {
+      return (
+        <div className="details">
+          <h2>Loading...</h2>
+        </div>
+      );
+    }
+
+    const { name, animal, breed, description, location } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 

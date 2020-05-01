82866f1c6c6c16324bb9bd7864ad036e0d1ad6b7
Enrico Massone <enrico.massone@deltatre.it>
2020-05-01 10:21:56 +0200

    added infrastructure to create modals

---
+++ src/modal.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createPortal } from "react-dom";
+
+const modalRoot = document.getElementById("modal-root");
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.containerElement = document.createElement("div");
+  }
+
+  componentDidMount() {
+    modalRoot.appendChild(this.containerElement);
+  }
+
+  componentWillUnmount() {
+    modalRoot.removeChild(this.containerElement);
+  }
+
+  render() {
+    return createPortal(this.props.children, this.containerElement);
+  }
+}
+
+export default Modal;
